ca85a084876c89ea8f542728ca07cfdf9eea35fb
Enrico Schwendig <enrico.schwendig@devbliss.com>
2016-09-11 21:02:52 +0200

    Change test process

--- tests/e2e/test/editor.js
+++ tests/e2e/test/editor.js
@@ -52,15 +52,9 @@ describe('The Editor', function () {
              expect(toolBar.getSaveButton().isEnabled()).toBe(false);
         });
 
-        it('should enable if text change', function () {
-            var el = container.getContentOfParagraphElement(2).click();
-            browser.actions().sendKeys('a').perform();
-            expect(toolBar.getSaveButton().getAttribute('disabled')).toEqual('disabled');
-            //expect(toolBar.getSaveButton().isEnabled()).toBe(true);
-
-        });
-
         it('should activate save handler', function () {
+            container.getContentOfParagraphElement(2).click();
+            browser.actions().sendKeys('a').perform();
             toolBar.getSaveButton().click();
             logs.expect(/Save from Angular!/);
         });
